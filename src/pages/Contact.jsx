6baa03bb3daf40a8f6be8289d9d95c2386dafadc
Ngene Samuel Obasi <samuelobasi2005@gmail.com>
2022-11-03 16:44:15 +0100

    Feat
    Created a new contact page using react-router-dom. Also styled the new page in the App.css file.

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "../App.css";
+const Contact = () => {
+  const name = "CERMUEL";
+  return (
+    <div className="contact">
+      <div className="heading">Contact Me</div>
+      <div className="supporting-text">
+        Hi there, contact me to ask me about anything you have in mind.
+      </div>
+      <form>
+        <div className="firstname">
+          <label htmlFor="first_name">First name</label>
+          <input
+            type="text"
+            id="first_name"
+            placeholder="Enter your first name"
+          />{" "}
+        </div>
+        <div className="lastname">
+          <label htmlFor="last_name">Last name</label>
+          <input
+            type="text"
+            id="last_name"
+            placeholder="Enter your last name"
+            required
+          />
+        </div>
+        <div className="other-inputs">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="yourname@email.com"
+            required
+          />
+        </div>
+        <div className="other-inputs">
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            placeholder="Send me a message and I'll reply you as soon as possible..."
+            required
+          />
+        </div>
+        <div className="checkbox-container">
+          <input type="checkbox" required />
+          <div>
+            You agree to providing your data to {name} who may contact you.
+          </div>
+        </div>
+        <button type="submit" id="btn__submit">
+          Send message
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
